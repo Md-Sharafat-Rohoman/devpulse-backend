3574d8b83a722a6d846d9aac22b5eda2ff01f4e3
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-05-24 05:47:46 +0600

    Database: feat: configure database connection with mongoose

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,19 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const config = {
-    port: process.env.PORT,
-    connection_string:process.env.CONNECTION_STRING as string,
-    jwt_secret:process.env.JWT_SECRET as string,
-    jwtExpiresIn:process.env.JWT_EXPIRESIN as string
-}
+// const config = {
+//     port: process.env.PORT,
+//     connection_string:process.env.CONNECTION_STRING as string,
+//     jwt_secret:process.env.JWT_SECRET as string,
+//     jwtExpiresIn:process.env.JWT_EXPIRESIN as string
+// }
 
 /* 
-Initial Commit: feat: initialize backend project structure
 
-Dependencies: chore: install express, dotenv, and cors dependencies
 
-Server Setup: feat: setup basic express server and port configuration
 
 Database: feat: configure database connection with mongoose
 
