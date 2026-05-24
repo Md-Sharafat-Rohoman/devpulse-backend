a2a6147597fa616a5e31665e5426385208fec843
Sharafat Rohoman <mdsharafatrohomanzafourllah@example.com>
2026-05-24 06:09:50 +0600

    vercel added

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { type Application, type Request, type Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { authRouter } from "./modules/auth/auth.route";
 import { issuesRouter } from "./modules/issues/issues.router";
+import { sendSuccess } from "./utils/response";
 const app: Application = express()
 
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    sendSuccess(res, StatusCodes.OK, 'Issue updated successfully')
 })
 
 app.use('/api/auth', authRouter);
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,28 +5,9 @@ dotenv.config();
 
 const config = {
     port: process.env.PORT,
-    connection_string:process.env.CONNECTION_STRING as string,
-    jwt_secret:process.env.JWT_SECRET as string,
-    jwtExpiresIn:process.env.JWT_EXPIRESIN as string
+    connection_string: process.env.CONNECTION_STRING as string,
+    jwt_secret: process.env.JWT_SECRET as string,
+    jwtExpiresIn: process.env.JWT_EXPIRESIN as string
 }
-
-/* 
-
-
-
-Database: feat: configure database connection with mongoose
-
-Middleware: feat: add error handling and logging middleware
-
-Model: feat: create base user schema and models
-
-Controller: feat: implement base controller logic for request handling
-
-Routes: feat: define initial API routes and endpoint structure
-
-Environment: chore: add environment variables template
-
-Cleanup: refactor: clean up project structure and remove unused files
-*/
 
 export default config;
